6e78a3430e36782f74ee2c37789b0dbe19f617ca
aka-nio <bruno.robim.nio@gmail.com>
2026-02-19 09:37:36 -0300

    solved the build import problem for the build run

--- src/db_operations/prismaInstance.ts
+++ src/db_operations/prismaInstance.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../../generated/prisma/index.js";
+import { PrismaClient } from "../../generated/prisma/index.js";
 
 let prismaInstance: PrismaClient | Record<string, unknown>;
 
